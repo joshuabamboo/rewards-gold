12c244e4aeb476521f6edf5d2df79639a6e895e9
github username <github email address>
2014-12-22 20:08:47 -0500

    it works

--- friend_inviter.rb
+++ friend_inviter.rb
@@ -2,21 +2,36 @@ require 'mechanize'
 require 'pry'
 
 class FriendGenerator
+  def initialize
+    @mechanize = Mechanize.new
+  end
 
-  def friend
-    mechanize = Mechanize.new
-    page = mechanize.get('http://www.rewardsgold.com/surv/referfriend.php')
-    form = mechanize.page.form_with(:name => "theform")
+  def login
+    page = @mechanize.get('http://www.rewardsgold.com/members/login.php')
+    form = @mechanize.page.form_with(:name => "theForm")
+    form.email_id = "josho2000@aol.com"
+    form.pwd = "joshua88"
+    button = form.button_with(:value => "submit")
+    @mechanize.submit(form, button)
+  end
+
+  def submit_friend_form
+    page = @mechanize.get('http://www.rewardsgold.com/surv/referfriend.php')
+    form = @mechanize.page.form_with(:name => "theform")
     form.FriendName1 = Random.rand(1000000000)
     form.FriendEmail1 = "#{Random.rand(1000000000)}@#{Random.rand(1000000000)}.com"
     form.FriendName2 = Random.rand(1000000000)
     form.FriendEmail2 = "#{Random.rand(1000000000)}@#{Random.rand(1000000000)}.com"
     button = form.button_with(:value => "Submit")
-    mechanize.submit(form, button)
+    @mechanize.submit(form, button)
     puts 'success'
+  end
+
+  def get_points
+    login
+    submit_friend_form
   end
 end
 
 f = FriendGenerator.new
-f.friend
-
+f.get_points
